e8b3c68a91bbd51622c534e11a1fcea674066040
Merilin Sel <merilin.sellik@gmail.com>
2019-12-03 01:07:44 +0200

    kalendri vaate esmane rough visuaal, modal, dropdown menu

--- application/views/pages/fullcalendar.php
+++ application/views/pages/fullcalendar.php
@@ -1,309 +1,59 @@
-
-<head>
-    <style>/*******************************
-* MODAL AS LEFT/RIGHT SIDEBAR
-* Add "left" or "right" in modal parent div, after class="modal".
-* Get free snippets on bootpen.com
-*******************************/
-	.modal.left .modal-dialog,
-	.modal.right .modal-dialog {
-		position: fixed;
-		margin: auto;
-		width: 320px;
-		height: 100%;
-		-webkit-transform: translate3d(0%, 0, 0);
-		    -ms-transform: translate3d(0%, 0, 0);
-		     -o-transform: translate3d(0%, 0, 0);
-		        transform: translate3d(0%, 0, 0);
-	}
-
-	.modal.left .modal-content,
-	.modal.right .modal-content {
-		height: 100%;
-		overflow-y: auto;
-	}
-	
-	.modal.left .modal-body,
-	.modal.right .modal-body {
-		padding: 15px 15px 80px;
-	}
-
-/*Left*/
-	.modal.left.fade .modal-dialog{
-		left: -320px;
-		-webkit-transition: opacity 0.3s linear, left 0.3s ease-out;
-		   -moz-transition: opacity 0.3s linear, left 0.3s ease-out;
-		     -o-transition: opacity 0.3s linear, left 0.3s ease-out;
-		        transition: opacity 0.3s linear, left 0.3s ease-out;
-	}
-	
-	.modal.left.fade.in .modal-dialog{
-		left: 0;
-	}
-        
-/*Right*/
-	.modal.right.fade .modal-dialog {
-		right: -320px;
-		-webkit-transition: opacity 0.3s linear, right 0.3s ease-out;
-		   -moz-transition: opacity 0.3s linear, right 0.3s ease-out;
-		     -o-transition: opacity 0.3s linear, right 0.3s ease-out;
-		        transition: opacity 0.3s linear, right 0.3s ease-out;
-	}
-	
-	.modal.right.fade.in .modal-dialog {
-		right: 0;
-	}
-
-/* ----- MODAL STYLE ----- */
-	.modal-content {
-		border-radius: 0;
-		border: none;
-	}
-
-	.modal-header {
-		border-bottom-color: #EEEEEE;
-		background-color: #FAFAFA;
-	}
-
-/* ----- v CAN BE DELETED v ----- */
-
-
-.demo {
-	padding-top: 60px;
-	padding-bottom: 110px;
-}
-
-.btn-demo {
-	margin: 15px;
-	padding: 10px 15px;
-	border-radius: 0;
-	font-size: 16px;
-	background-color: #FFFFFF;
-}
-
-.btn-demo:focus {
-	outline: 0;
-}
-
-.demo-footer {
-	position: fixed;
-	bottom: 0;
-	width: 100%;
-	padding: 15px;
-	background-color: #212121;
-	text-align: center;
-}
-
-.demo-footer > a {
-	text-decoration: none;
-	font-weight: bold;
-	font-size: 16px;
-	color: #fff;
-}</style>
-    <title>Jquery FullCalendar Integration with Codeigniter using Ajax</title>
-
-    <script>
-        $(document).ready(function() {
-
-            var calendar = $('#calendar').fullCalendar({
-                editable: true,
-                header: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'month,agendaWeek,agendaDay'
-                },
-                firstDay: 1,
-                dayNames: ['Pühapäev', 'Esmaspäev', 'Teisipäev', 'Kolmapäev',
-                    'Neljapäev', 'Reede', 'Laupäev'
-                ],
-                dayNamesShort: ['P', 'E', 'T', 'K', 'N', 'R', 'L'],
-                views: {
-                    week: { // name of view
-                        columnFormat: " ddd D.M"
-                        // other view-specific options here
-                    }
-                },
-
-                defaultView: 'agendaWeek',
-                weekNumbers: 'true',
-                slotLabelFormat: 'H:mm',
-                slotDuration: '00:15:00',
-                minTime: '08:00:00',
-                maxTime: '24:00:00',
-                //contentHeight:"auto",
-                events: "<?php echo base_url(); ?>fullcalendar/load/<?php echo ($this->input->get('roomId')); ?>",
-                selectable: true,
-                selectHelper: true,
-                select: function(start, end, allDay) {
-                    var public_info = prompt("Enter Event Title");
-                    var roomID = <?php echo ($this->input->get('roomId')); ?>;
-                    if (public_info) {
-                        var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
-                        var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
-                        $.ajax({
-                            url: "<?php echo base_url(); ?>fullcalendar/createfromcalendar",
-                            type: "POST",
-                            data: {
-                                roomID: roomID,
-                                public_info: public_info,
-                                start: start,
-                                end: end
-                            },
-                            success: function() {
-                                calendar.fullCalendar('refetchEvents');
-                                alert("Added Successfully");
-                            }
-                        })
-                    }
-                },
-                editable: true,
-                eventResize: function(event) {
-                    var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-                    var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-
-                    var title = event.title;
-
-                    var id = event.id;
-
-                    $.ajax({
-                        url: "<?php echo base_url(); ?>fullcalendar/update",
-                        type: "POST",
-                        data: {
-                            title: title,
-                            start: start,
-                            end: end,
-                            id: id
-                        },
-                        success: function() {
-                            calendar.fullCalendar('refetchEvents');
-                            alert("Event Update");
-                        }
-                    })
-                },
-                eventDrop: function(event) {
-                    var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-                    //alert(start);
-                    var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-                    //alert(end);
-                    var title = event.title;
-                    var id = event.id;
-                    $.ajax({
-                        url: "<?php echo base_url(); ?>fullcalendar/update",
-                        type: "POST",
-                        data: {
-                            title: title,
-                            start: start,
-                            end: end,
-                            id: id
-                        },
-                        success: function() {
-                            calendar.fullCalendar('refetchEvents');
-                            alert("Event Updated");
-                        }
-                    })
-                },
-                // eventClick: function(event) {
-                //     if (confirm("Are you sure you want to remove it?")) {
-                //         var id = event.id;
-                //         $.ajax({
-                //             url: "<?php echo base_url(); ?>fullcalendar/delete",
-                //             type: "POST",
-                //             data: {
-                //                 id: id
-                //             },
-                //             success: function() {
-                //                 calendar.fullCalendar('refetchEvents');
-                //                 alert('Event Removed');
-                //             }
-                //         })
-                //     }
-                // }
-
-                eventClick: function(event) {
-
-                    $("#successModal").modal("show");
-                    $("#successModal .modal-body p").text(event.title);
-                    $('#myModal').modal('toggle');
-                    $("#myModal .modal-body p").text(event.title);
-                }
-
-
-            });
-        });
-    </script>
-</head>
-
-<body>
-
-    <p align="right"><a href="https://tigu.hk.tlu.ee/~annemarii.hunt/codeigniter/booking/create">Broneerima</a></p>
-
     <div class="container">
-    <div id='calendar'></div>
-    <div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="successModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <p></p>
+        <div class="d-flex pt-4 pb-2">
+            <form class="d-flex flex-row col-10 p-0" action="fullcalendar" method="get">
+                <div class="form-label-group col-4 p-0">
+                    <label for="room">Saal</label>
+                    <input id="room" list="saal" class="form-control arrow">
+                    <datalist id="saal">
+                        <?php foreach ($rooms as $each) {
+                            echo '<option data-value="' . $each->id . '">' . $each->name . '</option>';
+                        }
+                        ?>
+
+                    </datalist>
+                    <input type="hidden" id="roomId" name="roomId" value="roomId" />
                 </div>
-            </div>
+ 
+                <div class="form-label-group col-2">
+                    <label for="app">Kuupäev</label>
+                    <div id='app'>
+                        <v-date-picker mode="single" v-model="date" :popover="{ visibility: 'click' }">
+                            <input id="date" slot-scope="{ inputProps, inputEvents}" :class="[`form-control`]" v-bind="inputProps" v-on="inputEvents" name="date" value="date">
+                        </v-date-picker>
+                    </div>
+                </div>
+            </form>
+            <a class="btn btn-custom col-2 text-white mr-auto p-0 mb-4 text-center align-middle" href="<?php echo base_url(); ?>booking/create">Broneerima</a>
+                    </div>
+
+                        <!-- ekfjwkejfkwejÄ -->
+
+        <div class="calendar-container">
+            <div id='calendar'></div>
         </div>
-    </div>
 
+        </br>
 
-    <div class="container demo">
-	
-	
-	<div class="text-center">
-		<button type="button" class="btn btn-demo" data-toggle="modal" data-target="#myModal">
-			Left Sidebar Modal
-		</button>
+	<!-- Modal -->
+	<div class="modal left" id="lefty" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
 
-		<button type="button" class="btn btn-demo"  data-target="#myModal2">
-			Right Sidebar Modal
-		</button>
+				<div class="modal-header">
+                <h4 class="modal-title" id="myModalLabel">Left Sidebar</h4>
+
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					
+				</div>
+
+				<div class="modal-body">
+					<p id="time"></p>
+				</div>
+
+			</div>
+		</div>
 	</div>
 
-	<!-- Modal -->
-	<div class="modal left fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h4 class="modal-title" id="myModalLabel">Left Sidebar</h4>
-				</div>
-
-				<div class="modal-body">
-					<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-					</p>
-				</div>
-
-			</div><!-- modal-content -->
-		</div><!-- modal-dialog -->
-	</div><!-- modal -->
-	
-	<!-- Modal -->
-	<div class="modal right fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h4 class="modal-title" id="myModalLabel2">Right Sidebar</h4>
-				</div>
-
-				<div class="modal-body">
-					<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-					</p>
-				</div>
-
-			</div><!-- modal-content -->
-		</div><!-- modal-dialog -->
-	</div><!-- modal -->
-	
 	
 </div><!-- container -->
 
@@ -312,4 +62,155 @@
     </div>
 </body>
 
-</html>
+<script src='https://unpkg.com/v-calendar@next'></script>
+
+<script>
+    new Vue({
+        el: '#app',
+        data: {
+            // Data used by the date picker
+            mode: 'single',
+            selectedDate: null,
+            firstDayOfWeek: 2,
+
+            attrs: [{
+                key: 'today',
+                highlight: true,
+                dates: new Date(),
+            }, ],
+            date: new Date(),
+        }
+    });
+
+            $(document).ready(function() {
+
+var calendar = $('#calendar').fullCalendar({
+    editable: false,
+    header: {
+        left: '',
+        center: 'prev, title, next',
+        right: ''
+    },
+    height: 'parent',
+    allDaySlot: false,
+    firstDay: 1,
+    dayNames: ['Pühapäev', 'Esmaspäev', 'Teisipäev', 'Kolmapäev', 'Neljapäev', 'Reede', 'Laupäev'],
+    dayNamesShort: ['P', 'E', 'T', 'K', 'N', 'R', 'L'],
+    monthNames: ['jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember'],
+    monthNamesShort: ['jaan', 'veebr', 'märts', 'apr', 'mai', 'juuni', 'juuli', 'aug', 'sept', 'okt', 'nov', 'dets'],
+    views: {
+        week: { // name of view
+            titleFormat: 'D. MMMM YYYY',
+            columnFormat: "dddd, D. MMM"
+            // other view-specific options here
+        },
+    },
+
+    defaultView: 'agendaWeek',
+    weekNumbers: false,
+    slotLabelFormat: 'H:mm',
+    slotDuration: '00:30:00',
+    minTime: '08:00:00',
+    maxTime: '22:00:00',
+    //contentHeight:"auto",
+    events: "<?php echo base_url(); ?>fullcalendar/load/<?php echo ($this->input->get('roomId')); ?>",
+    selectable: false,
+    selectHelper: true,
+    select: function(start, end, allDay) {
+        var public_info = prompt("Enter Event Title");
+        var roomID = <?php echo ($this->input->get('roomId')); ?>;
+        if (public_info) {
+            var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+            var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+            $.ajax({
+                url: "<?php echo base_url(); ?>fullcalendar/createfromcalendar",
+                type: "POST",
+                data: {
+                    roomID: roomID,
+                    public_info: public_info,
+                    start: start,
+                    end: end
+                },
+                success: function() {
+                    calendar.fullCalendar('refetchEvents');
+                    alert("Added Successfully");
+                }
+            })
+        }
+    },
+    editable: true,
+    eventResize: function(event) {
+        var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+        var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+
+        var title = event.title;
+
+        var id = event.id;
+
+        $.ajax({
+            url: "<?php echo base_url(); ?>fullcalendar/update",
+            type: "POST",
+            data: {
+                title: title,
+                start: start,
+                end: end,
+                id: id
+            },
+            success: function() {
+                calendar.fullCalendar('refetchEvents');
+                alert("Event Update");
+            }
+        })
+    },
+    eventDrop: function(event) {
+        var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+        //alert(start);
+        var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+        //alert(end);
+        var title = event.title;
+        var id = event.id;
+        $.ajax({
+            url: "<?php echo base_url(); ?>fullcalendar/update",
+            type: "POST",
+            data: {
+                title: title,
+                start: start,
+                end: end,
+                id: id
+            },
+            success: function() {
+                calendar.fullCalendar('refetchEvents');
+                alert("Event Updated");
+            }
+        })
+    },
+    // eventClick: function(event) {
+    //     if (confirm("Are you sure you want to remove it?")) {
+    //         var id = event.id;
+    //         $.ajax({
+    //             url: "<?php echo base_url(); ?>fullcalendar/delete",
+    //             type: "POST",
+    //             data: {
+    //                 id: id
+    //             },
+    //             success: function() {
+    //                 calendar.fullCalendar('refetchEvents');
+    //                 alert('Event Removed');
+    //             }
+    //         })
+    //     }
+    // }
+
+    eventClick: function(event) {
+
+        // $("#successModal").modal("show");
+        // $("#successModal .modal-body p").text(event.title);
+        $('#lefty').modal('show');
+        $("#lefty .modal-header h4").text(event.title);
+        $("#lefty #time").text(event.start);
+    }
+
+
+});
+});
+</script>
