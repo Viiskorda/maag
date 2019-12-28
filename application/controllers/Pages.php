19ce10a19cb0548fb4c89609b2534591dd48f1c9
Annemarii Hunt <annemarii.hunt@gmail.com>
2019-12-28 20:32:09 +0200

    TEgin redirecti pealehelt kui urlis midagi valesti

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -11,7 +11,8 @@ class Pages extends CI_Controller
         {
                 if (!file_exists(APPPATH . 'views/pages/' . $page . '.php')) {
                         // Whoops, we don't have a page for that!
-                        show_404();
+                        redirect('');
+                      //  show_404();
                 }
               
                 $data['title'] = ucfirst($page); // Capitalize the first letter
